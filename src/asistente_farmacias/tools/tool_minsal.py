06c84e9373fd09f08faf31cea032cf4f6a04d30c
supermaster1982 <52577161+supermaster1982@users.noreply.github.com>
2026-08-16 10:55:56 -0400

    Update URLs for turnos and locales in tool_minsal.py
    Comment out old URLs and update to new proxy URLs.

--- src/asistente_farmacias/tools/tool_minsal.py
+++ src/asistente_farmacias/tools/tool_minsal.py
@@ -26,8 +26,8 @@ from pathlib import Path
 import requests
 from langchain_core.tools import tool
 
-//URL_TURNOS = "https://midas.minsal.cl/farmacia_v2/WS/getLocalesTurnos.php"
-//URL_LOCALES = "https://midas.minsal.cl/farmacia_v2/WS/getLocales.php"
+#URL_TURNOS = "https://midas.minsal.cl/farmacia_v2/WS/getLocalesTurnos.php"
+#URL_LOCALES = "https://midas.minsal.cl/farmacia_v2/WS/getLocales.php"
 URL_TURNOS = "https://minsal-proxy.alexis-contrerasnaranjo.workers.dev/turnos"
 URL_LOCALES = "https://minsal-proxy.alexis-contrerasnaranjo.workers.dev/locales"
 
